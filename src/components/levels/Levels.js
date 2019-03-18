ddbe9555e06641af2852b4a4bf631023ff8bd6c2
Lee Phan <lphan@iherb.net>
2019-03-18 13:58:40 -0700

    Added dict for lv title, component, and lvMsg

--- src/components/levels/Levels.js
+++ src/components/levels/Levels.js
@@ -10,28 +10,22 @@ export default function Levels(){
 
   const [currentLevel, setCurrentLevel] = useState(0);
 
-  const titles = [
-    "Level: 0",
-    "Level: 1",
-    "Level: 2",
-    "Level: 3",
-  ]
-
   const onLevelPassed = () => {
       setCurrentLevel(currentLevel + 1);
   }
 
   const dictLevel = {
-    0: <PreLevel onLevelPassed={onLevelPassed} />,
-    1: <FirstLevel onLevelPassed={onLevelPassed} />,
-    2: <SecondLevel onLevelPassed={onLevelPassed} />,
-    3: <ThirdLevel onLevelPassed={onLevelPassed} />,
+    0: { component: <PreLevel onLevelPassed={onLevelPassed} />, title: "0", lvMsg: "abc" },
+    1: { component: <PreLevel onLevelPassed={onLevelPassed} />, title: "1", lvMsg: "abc" },
+    2: { component: <PreLevel onLevelPassed={onLevelPassed} />, title: "2", lvMsg: "abc" },
+    3: { component: <PreLevel onLevelPassed={onLevelPassed} />, title: "3", lvMsg: "abc" },
   }
 
   return(
     <div>
-        <h1 className="text-center">{ titles[currentLevel] }</h1>
-        { dictLevel[currentLevel] }
+        <h1 className="text-center">Level: { dictLevel[currentLevel].title }</h1>
+        { dictLevel[currentLevel].component }
+    <div className="border p-2 m-2">{dictLevel[currentLevel].lvMsg}</div>
     </div>
   )
 }
