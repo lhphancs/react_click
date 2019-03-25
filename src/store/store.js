9c7fa89a16d28d1c99114c9d6d3370ae43d3472d
Lee Phan <lphan@iherb.net>
2019-03-25 14:11:34 -0700

    Made compatible with ReduxDevTool

--- src/store/store.js
+++ src/store/store.js
@@ -3,5 +3,6 @@ import { createStore, applyMiddleware } from 'redux'
 
 const store = createStore(
     Reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 export default store;
